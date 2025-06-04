bb1a9d1b0195172ffccd63f9d96c6b0afbde161f
fabian serkeyn <fserkeyn@tv115aiworkstation.clo.be>
2025-06-04 09:44:52 +0200

    push all files, training finished

--- main.py
+++ main.py
@@ -6,7 +6,38 @@
 from ultralytics import YOLO
 
 # Load a pretrained YOLO11 segment model
-model = YOLO("yolo11n-seg.pt")
+model = YOLO("yolo11s-seg.pt")
 
 # Train the model
-results = model.train(data="fine-dataset.yaml", epochs=100, batch=16, imgsz=640, save=True, device=0, project="yolo11-fine", name="test-train-1", exist_ok=False)
+results = model.train(
+                        data="fine-dataset.yaml", 
+                        epochs=300, 
+                        patience=60, 
+                        batch=48, 
+                        imgsz=832, 
+                        lr0=0.001,
+                        lrf=0.01, 
+                        save=True, 
+                        device=[0,1], 
+                        project="yolo11-fine", 
+                        name="test-train-11",
+                        cos_lr=True, 
+                        exist_ok=False, 
+                        dropout=0.1, 
+                        mixup=0.0, 
+                        copy_paste=0.2, 
+                        hsv_h=0.02, 
+                        hsv_s=0.2, 
+                        hsv_v=0.2, 
+                        scale=0.7, 
+                        degrees=10, 
+                        translate=0.3, 
+                        shear=30, 
+                        perspective=0.001, 
+                        flipud=0.15,
+                        fliplr=0.15, 
+                        mosaic=0.4,
+                        warmup_epochs=5,
+                        warmup_momentum=0.8,
+                        warmup_bias_lr=0.1
+                        )
